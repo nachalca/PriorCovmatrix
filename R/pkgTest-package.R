e71315c43f235da99efb06e880ba7187ac90929a
nachalca <nachalca@gmail.com>
2018-08-29 22:47:09 -0300

    big update

---
+++ R/pkgTest-package.R
@@ -0,0 +1,18 @@
+#' The 'PriorCovmatrix' package.
+#' 
+#' @description Simulate and visualize covariance matrix distributions
+#' 
+#' @docType package
+#' @name PriorCovmatrix-package
+#' @aliases PriorCovmatrix
+#' @useDynLib PriorCovmatrix, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @import rstantools
+#' @importFrom rstan sampling
+#' @importFrom magrittr %>%
+#' 
+#' @references 
+#' Stan Development Team (2018). RStan: the R interface to Stan. R package version 2.17.3. http://mc-stan.org
+#' 
+NULL
